3f8cef0cc7e1a7bee34460f9b71aa720ed7da78c
aviranGoel <62604949+aviranGoel@users.noreply.github.com>
2021-09-19 12:05:10 +0300

    Update README file, index.js
    Update README file, index.js

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Yammie
+# Yammie Restaurant
+
+## Welcome to Yammie Restaurant Backend API!
+
+### A Backend Rest API which built in JavaScript and node.js, and runs locally.
+### The server can receive the following requests from the client:
+
+1. Save a new order.
+   
+   By receiving a Post request that contains the order details within the body request:
+
+   "http://localhost:3000/save"
+
+2. Get all orders from the last day.
+
+   By receiving a Get request:
+   
+   "http://localhost:3000/load"
+
+### The orders are saved in the json file: "yammieOrdersDb.json".
+
+### The requests are documented in a log file: "yammieAPI_logger.log".
+
+---
+## Instructions:
+
+To get the YammieBackendAPI app running locally, follow these steps:
+
+1. Clone the [Yammie](https://github.com/aviranGoel/Yammie) repository.
+2. Install the node modules `npm install`.
+3. Run the app `npm start`.
+4. The app available at `http://localhost:3000`.
+5. Access the paths mentioned above by using `Postman`.
--- index.js
+++ index.js
@@ -30,10 +30,45 @@ app.use("/load", load);
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Welcome to Yammie Backend API</h1>"
+    "<h1>Welcome to Yammie Restaurant Restaurant Backend API!</h1>" +
+      "<h3>A Backend Rest API which built in JavaScript and node.js, and runs locally." +
+      "<br/>" +
+      "The server can receive the following requests from the client:</h3>" +
+      "<p>&emsp;<b>1. Save a new order.</b>" +
+      "<br/>" +
+      "&emsp;&emsp;" +
+      "By receiving a Post request that contains the order details within the body request:" +
+      "<br/>" +
+      "&emsp;&emsp;" +
+      '"http://localhost:3000/save"</p>' +
+      "<p>&emsp;<b>2. Get all orders from the last day.</b>" +
+      "<br/>" +
+      "&emsp;&emsp;" +
+      "By receiving a Get request:" +
+      "<br/>" +
+      "&emsp;&emsp;" +
+      '"http://localhost:3000/load"</p>' +
+      "<h3>The orders are saved in the json file: 'yammieOrdersDb.json'." +
+      "<br/>" +
+      "The requests are documented in a log file: 'yammieAPI_logger.log'.</h3>" +
+      "<hr>" +
+      "<h2>Instructions:</h2>" +
+      "<p>To get the YammieBackendAPI app running locally, follow these steps:" +
+      "<br/>" +
+      "1. Clone the <a href='https://github.com/aviranGoel/Yammie'>Yammie</a> repository." +
+      "<br/>" +
+      "2. Install the node modules `npm install`." +
+      "<br/>" +
+      "3. Run the app `npm start`." +
+      "<br/>" +
+      "4. The app available at `http://localhost:3000`." +
+      "<br/>" +
+      "5. Access the paths mentioned above by using `Postman`.</p>"
   );
 });
 
 app.listen(port, () => {
-  console.log(`Yammie Backend API app listening at http://localhost:${port}`);
+  console.log(
+    `Yammie Restaurant Backend API app listening at http://localhost:${port}`
+  );
 });
